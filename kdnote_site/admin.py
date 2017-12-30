3067dfc6d4c8cd2d0263719f151fdb1f676522ca
YutaroKikuchi <yusan@YutaroKikuchinoMacBook-puro.local>
2017-12-30 17:52:57 +0900

    ユーザ用のデータベースを作成

--- kdnote_site/admin.py
+++ kdnote_site/admin.py
@@ -9,12 +9,12 @@ class UserAdmin(BaseUserAdmin):
 	# The fields to be used in displaying the User model.
 	# These override the definitions on the base UserAdmin
 	# that reference specific fields on auth.User.
-	list_display = ('email','user_name', 'is_admin', 'create_date', 'update_date')
+	list_display = ('email','user_name', 'is_staff', 'is_admin', 'create_date', 'update_date')
 	list_filter = ()
 	fieldsets = (
 		(None, {'fields': ('email', 'password')}),
 		('user_name', {'fields': ('user_name',)}),
-		('Permissions', {'fields': ('is_admin',)}),
+		('Permissions', {'fields': ('is_admin','is_staff',)}),
 		('date', {'fields': ('create_date', 'update_date')}),
 	)
 	search_fields = ('email',)
--- kdnote_site/models.py
+++ kdnote_site/models.py
@@ -6,14 +6,14 @@ from django.utils import timezone
 # Create your models here.
 
 class UserManager(BaseUserManager):
-	def create_user(self, email, username, password):
+	def create_user(self, email, user_name, password):
 		"""
 		Creates and saves a User with the given email and password.
 		"""
 		if not email:
 			raise ValueError('Users must have an email address')
 
-		if not username :
+		if not user_name :
 			raise ValueError('Users must have an user name')
 
 		if not password :
@@ -21,24 +21,24 @@ class UserManager(BaseUserManager):
 
 		user = self.model(
 			email=self.normalize_email(email),
-			user_name=username,
+			user_name=user_name,
 			password=password
 		)
 
 		user.set_password(password)
 		user.set_create_date()
-		user.set_update_date(timezone.now)
+		user.set_update_date(timezone.now())
 		user.save(using=self._db)
 		return user
 
-	def create_superuser(self, email, username, password):
+	def create_superuser(self, email, user_name, password):
 		"""
 		Creates and saves a User with the given email and password.
 		"""
 		if not email:
 			raise ValueError('Users must have an email address')
 
-		if not username :
+		if not user_name :
 			raise ValueError('Users must have an user name')
 
 		if not password :
@@ -46,14 +46,15 @@ class UserManager(BaseUserManager):
 
 		user = self.model(
 			email=self.normalize_email(email),
-			user_name=username,
+			user_name=user_name,
 			password=password
 		)
 
+		user.is_staff = True
 		user.set_admin()
 		user.set_password(password)
 		user.set_create_date()
-		user.set_update_date(timezone.now)
+		user.set_update_date(timezone.now())
 		user.save(using=self._db)
 		return user
 
@@ -65,6 +66,7 @@ class CustomUser(AbstractBaseUser):
 		primary_key=True
 	)
 	USERNAME_FIELD='email'
+	is_staff = models.BooleanField(default=False)
 	is_admin = models.BooleanField(default=False)
 	user_name = models.CharField(verbose_name='user name',max_length=255)
 	create_date = models.DateField()
@@ -81,10 +83,29 @@ class CustomUser(AbstractBaseUser):
 		return self.user_name
 
 	def set_create_date(self):
-		self.create_date = timezone.now
+		self.create_date = timezone.now()
 	
 	def set_update_date(self, date):
 		self.update_date = date
 	
 	def set_admin(self):
 		self.is_admin = True
+
+	def __str__(self):
+		return self.email
+
+	def has_perm(self, perm, obj=None):
+		"Does the user have a specific permission?"
+		# Simplest possible answer: Yes, always
+		return True
+
+	def has_module_perms(self, app_label):
+		"Does the user have permissions to view the app `app_label`?"
+		# Simplest possible answer: Yes, always
+		return True
+	
+	@property
+	def is_staff(self):
+		"Is the user a member of staff?"
+		# Simplest possible answer: All admins are staff
+		return self.is_admin
