b7e603e7b7ab2b295b56e704f44e25d01a0e55f6
YutaroKikuchi <yusan@YutaroKikuchinoMacBook-puro.local>
2017-12-30 19:58:04 +0900

    ログイン画面用のビュー関数を変更 mypageを少し変更

--- kdnote_site/urls.py
+++ kdnote_site/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.login, name='login'),
+    url(r'^$', views.login_phase, name='login'),
 	url(r'^mypage/$', views.mypage, name="mypage"),
 ]
--- kdnote_site/views.py
+++ kdnote_site/views.py
@@ -9,15 +9,16 @@ from .models import CustomUser
 
 # Create your views here.
 
-#def login(request):
-#	return render(request, 'kdnote_site/login.html',{})
+def login_phase(request):
+	return render(request, 'kdnote_site/login.html',{})
 
 def mypage(request):
 	email = request.POST['email']
 	passward = request.POST['password']
 	user = authenticate(request,email=email,password=passward)
 	if user is not None:
+		context = {"user_name":user.user_name,"email":user.email, "create_date":user.create_date, "update_date":user.update_date}
 		login(request,user)
-		return render(request, 'kdnote_site/mypage.html',{})
+		return render(request, 'kdnote_site/mypage.html',context)
 	else:
 		raise Http404("user does not exist")
