bf072d75d01522305c4e36c8d589354c3969b7c9
YutaroKikuchi <yusan@YutaroKikuchinoMacBook-puro.local>
2017-12-30 19:28:39 +0900

    メールアドレスとパスワードでマイページに遷移できるようになった

--- kdnote_site/views.py
+++ kdnote_site/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.http import Http404
 
 from .models import CustomUser
 
 # Create your views here.
 
-def login(request):
-	return render(request, 'kdnote_site/login.html',{})
+#def login(request):
+#	return render(request, 'kdnote_site/login.html',{})
 
 def mypage(request):
-	login_id = request.POST.get('email')
-	userdata = get_object_or_404(CustomUser, pk=login_id)
-
-	return render(request, 'kdnote_site/mypage.html',{})
+	email = request.POST['email']
+	passward = request.POST['password']
+	user = authenticate(request,email=email,password=passward)
+	if user is not None:
+		login(request,user)
+		return render(request, 'kdnote_site/mypage.html',{})
+	else:
+		raise Http404("user does not exist")
